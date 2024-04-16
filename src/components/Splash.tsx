eaed1af888e0e059c24ae2b35e9dd5b865af24fa
Antony Lajes <tonymateuslajes@hotmail.com>
2024-04-16 11:38:49 -0300

    add - Criada a UI da tela Splash

--- src/components/Splash.tsx
+++ src/components/Splash.tsx
@@ -1,9 +1,29 @@
-import { Text } from "native-base"
+import { Text, VStack } from "native-base"
+import LogoSvg from "@assets/logo.svg"
 
 export function Splash(){
 
     return (
-        <Text>Splash</Text>
+        <VStack
+            bg={"gray.700"}
+            flex={1}
+            justifyContent={"center"}
+            alignItems={"center"}
+            space={9}
+        >
+            <LogoSvg
+                height={130}
+                width={150}
+            />
+            <Text
+                fontSize={"xxl"}
+                textTransform={"uppercase"}
+                fontFamily={"black"}
+                color={"green.600"}
+            >
+                ABASTECIMENTO
+            </Text>
+        </VStack>
     )
 
 }
