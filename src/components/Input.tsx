17efc2c89c8f57777647f868bdccdc484bf3d91c
Antony Lajes <tonymateuslajes@hotmail.com>
2024-04-16 12:14:34 -0300

    update - Gerenciamento de campos da tela SignIn com React Hook Form

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,37 +1,50 @@
-import { IInputProps, Input as NativeBaseInput, Text, VStack } from 'native-base'
+import { IInputProps, Input as NativeBaseInput, Text, VStack, FormControl } from 'native-base'
 
 type Props = IInputProps & {
-    title: string
+    title: string,
+    errorMessage ?: string | null
 }
 
-export function Input({title, ...rest}: Props){
+export function Input({errorMessage = null, isInvalid, title, ...rest}: Props){
+
+    const invalid = !!errorMessage || isInvalid
 
     return (
         <VStack
             space={1}
         >
             <Text
-                color={"gray.100"}
+                color={invalid ? "error.600" :"gray.100"}
                 fontSize={"sm"}
             >
                 {title}
             </Text>
-            <NativeBaseInput
-                bg={"gray.800"}
-                p={2}
-                borderWidth={1}
-                borderRadius={8}
-                borderColor={"gray.500"}
-                fontSize={"sm"}
-                color={"gray.100"}
-                placeholder={title}
-                placeholderTextColor={"gray.500"}
-                _focus={{
-                    borderColor: "green.600",
-                    bg: "gray.800"
-                }}
-                {...rest}
-            />
+            <FormControl
+                isInvalid={invalid}
+            >
+                <NativeBaseInput
+                    bg={"gray.800"}
+                    p={2}
+                    borderWidth={1}
+                    borderRadius={8}
+                    borderColor={"gray.500"}
+                    fontSize={"sm"}
+                    color={"gray.100"}
+                    placeholder={title}
+                    placeholderTextColor={"gray.500"}
+                    _focus={{
+                        borderColor: "green.600",
+                        bg: "gray.800"
+                    }}
+                    _invalid={{
+                        borderColor: "error.600"
+                    }}
+                    {...rest}
+                />
+                <FormControl.ErrorMessage>
+                    {errorMessage}
+                </FormControl.ErrorMessage>
+            </FormControl>
         </VStack>
     )
 
